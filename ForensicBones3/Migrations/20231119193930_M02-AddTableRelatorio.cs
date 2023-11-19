1169c47f798fe1c6ef284eb6c54a9520aeb3b37e
João Paulo de Freitas Teixeira Leite <joao.leite.740407@sga.pucminas.br>
2023-11-19 17:22:32 -0300

    Todos os models, exceto Marcadores (sem migration, exceto Usuarios)

---
+++ ForensicBones3/Migrations/20231119193930_M02-AddTableRelatorio.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ForensicBones3.Migrations
+{
+    public partial class M02AddTableRelatorio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- ForensicBones3/Migrations/20231119202026_M03-AddTable-Descricao-Esqueleto-Cranio.Designer.cs
+++ ForensicBones3/Migrations/20231119202026_M03-AddTable-Descricao-Esqueleto-Cranio.Designer.cs
@@ -4,6 +4,7 @@ using ForensicBones3.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ForensicBones3.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231119202026_M03-AddTable-Descricao-Esqueleto-Cranio")]
+    partial class M03AddTableDescricaoEsqueletoCranio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
